3fcc7b4c6e3a7cde7a246f8196ea5af88138ae4d
Riccardo Biraghi <riccardo.biraghi@ibm.com>
2018-04-10 13:42:44 +0100

    Return nil in checkFS

--- cmd/runmqserver/mqconfig_other.go
+++ cmd/runmqserver/mqconfig_other.go
@@ -20,5 +20,5 @@ package main
 // Dummy version of this function, only for non-Linux systems.
 // Having this allows unit tests to be run on other platforms (e.g. macOS)
 func checkFS(path string) error {
-	return
+	return nil
 }
