3559cce45cb92e68e51647a1f7b3c574576338f5
Robert Parker <parrobe@uk.ibm.com>
2018-10-08 16:57:07 +0100

    update apparmor

--- install-mq.sh
+++ install-mq.sh
@@ -139,7 +139,7 @@ rm -rf ${DIR_EXTRACT}
 
 # Apply any bug fixes not included in base Ubuntu or MQ image.
 # Don't upgrade everything based on Docker best practices https://docs.docker.com/engine/userguide/eng-image/dockerfile_best-practices/#run
-$UBUNTU && apt-get install -y gcc-5-base gnupg gpgv libgcrypt20 libstdc++6 perl-base --only-upgrade
+$UBUNTU && apt-get install -y libapparmor1 --only-upgrade
 # End of bug fixes
 
 # Clean up cached files
