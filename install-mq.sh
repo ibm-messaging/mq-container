bfaf6a7986e59856579e8d7b6d8029a73bf1417b
Robert Parker <parrobe@uk.ibm.com>
2018-11-07 15:22:28 +0000

    Security fixes, Nov 2018

--- install-mq.sh
+++ install-mq.sh
@@ -139,7 +139,7 @@ rm -rf ${DIR_EXTRACT}
 
 # Apply any bug fixes not included in base Ubuntu or MQ image.
 # Don't upgrade everything based on Docker best practices https://docs.docker.com/engine/userguide/eng-image/dockerfile_best-practices/#run
-$UBUNTU && apt-get install -y libapparmor1 --only-upgrade
+$UBUNTU && apt-get install -y libapparmor1 libsystemd0 systemd systemd-sysv libudev1 --only-upgrade
 # End of bug fixes
 
 # Clean up cached files
