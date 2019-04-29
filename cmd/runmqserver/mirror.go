35a32179f79e2ec02528c1b4dcabb9c048fb4d9a
Stephen Marshall <stmarsh@uk.ibm.com>
2019-04-29 14:33:05 +0100

    Update copyrights

--- cmd/runmqserver/mirror.go
+++ cmd/runmqserver/mirror.go
@@ -1,5 +1,5 @@
 /*
-© Copyright IBM Corporation 2018
+© Copyright IBM Corporation 2018, 2019
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -22,6 +22,8 @@ import (
 	"os"
 	"sync"
 	"time"
+
+	"github.com/prometheus/common/log"
 )
 
 // waitForFile waits until the specified file exists
--- cmd/runmqserver/mirror_test.go
+++ cmd/runmqserver/mirror_test.go
@@ -1,5 +1,5 @@
 /*
-© Copyright IBM Corporation 2018
+© Copyright IBM Corporation 2018, 2019
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"strconv"
 	"strings"
