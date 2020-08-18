666b23e7f0cb6b7b870a816e1bc7cf2e803d0148
KIRAN DARBHA <kirandarbha@in.ibm.com>
2020-08-18 16:49:01 +0530

    adding zlinux-support

--- travis-build-scripts/push.sh
+++ travis-build-scripts/push.sh
@@ -32,7 +32,7 @@ function push_developer {
 }
 
 function push_production {
-    if [ "$ARCH" = "amd64" ] ; then
+    if [[ "$ARCH" = "amd64" || "$ARCH" = "s390x" ]] ; then
         echo 'Pushing Production image...' && echo -en 'travis_fold:start:push-advancedserver\\r'
         make push-advancedserver
         echo -en 'travis_fold:end:push-advancedserver\\r'
