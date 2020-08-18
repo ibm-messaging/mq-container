7c896067826faaada86e99c7c4123b136aed3ce1
KIRAN DARBHA <kirandarbha@in.ibm.com>
2020-08-18 17:22:26 +0530

    adding zlinux-support

--- travis-build-scripts/test.sh
+++ travis-build-scripts/test.sh
@@ -20,14 +20,14 @@ echo 'Testing Developer image...' && echo -en 'travis_fold:start:test-devserver\
 make test-devserver
 echo -en 'travis_fold:end:test-devserver\\r'
 if [ "$BUILD_ALL" = true ] ; then
-    if [ "$ARCH" = "amd64" ] ; then
+    if [[ "$ARCH" = "amd64" || "$ARCH" = "s390x" ]] ; then
         echo 'Testing Production image...' && echo -en 'travis_fold:start:test-advancedserver\\r'
         make test-advancedserver
         echo -en 'travis_fold:end:test-advancedserver\\r'
     fi
 fi
 echo 'Running gosec scan...' && echo -en 'travis_fold:start:gosec-scan\\r'
-if [ "$ARCH" = "amd64" ] ; then
+if [[ "$ARCH" = "amd64" || "$ARCH" = "s390x" ]] ; then
     make gosec
 else
     echo "Gosec not available on ppc64le/s390x...skipping gosec scan"
