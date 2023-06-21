6f4b15769fb8cd3bf84a17889a87598011520051
Alec Painter <Alec.Painter@ibm.com>
2023-06-21 10:46:07 +0100

    Ahp-travis-migration-933 (#470)
    * Added a keep alive script to stop travis timing out
    
    * Added travis 3 multi-arch support

---
+++ travis-build-scripts/travis-log-keepalive.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# © Copyright IBM Corporation 2020
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+k=0
+dd=$(date)
+
+# Output 200 lines of dummy output to force the logs to flush if they have broken
+while true; do
+    echo -e "travis_fold:start:build_heartbeat.$k\033[33;1mDumping heartbeat logs to keep build alive - $dd\033[0m"
+    for i in {1..200}; do 
+        echo "Keepalive $i"
+    done
+    echo -e "\ntravis_fold:end:build_heartbeat.$k\r"
+    sleep 120
+    k=$((k+1))
+    dd=$(date)
+done
