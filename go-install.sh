8c28596df7a0225a19f66357825998e572eb9dbb
Devipriya Selvarajan <devipriya.selvarajan1@ibm.com>
2025-01-07 15:25:57 +0530

    Remove gimme and update group to vpc

--- go-install.sh
+++ go-install.sh
@@ -19,9 +19,9 @@
 
 set -ex
 
-sudo rm -rf /usr/local/go;
+sudo rm -rf /usr/local/go
 DOWNLOAD_URL="https://go.dev/dl/go${GO_VERSION}.linux-${ARCH}.tar.gz"
 curl -fLo go.tar.gz "${DOWNLOAD_URL}"
 sudo tar -C /usr/local -xzf go.tar.gz
-export PATH=$PATH:/usr/local/go/bin
+export PATH=/usr/local/go/bin:$PATH
 go version
